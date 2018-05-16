f89cf359c77666169fad5128843190c9b549d390
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-05-16 12:34:41 +0200

    fix urls

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ const httpLink = createHttpLink({
 })
 
 const wsLink = new WebSocketLink({
-  uri: process.env.SERVER_URL || 'ws://localhost:3000/subscriptions',
+  uri: process.env.SERVER_URL_SUB || 'ws://localhost:3000/subscriptions',
   options: {
     reconnect: true
   }
